b1ea3ab03d36f6453b4ef2eecd61be4a254f200f
Brendon W. <bwherry@midland.k12.ia.us>
2026-02-13 12:21:56 -0600

    Update Account.cs

--- Account.cs
+++ Account.cs
@@ -2,7 +2,7 @@
 
 public class Account {
 
-      public string Name;
+      public string[] Name = ("William", "Afton");
          
       
 
@@ -14,6 +14,7 @@ public class Account {
          
       }
 }
+
 
 
 
